d3e09a071daf43606d3731f04dedfc383fcccfd3
Sylvain Guérin <sylvain@openflexo.org>
2016-08-29 10:33:34 +0200

    LOW / Added SysMLKaos VP

--- src/main/java/org/openflexo/modelers/ModelersConstants.java
+++ src/main/java/org/openflexo/modelers/ModelersConstants.java
@@ -5,4 +5,6 @@ public class ModelersConstants {
 	public static final String MODELERS_RC_URI = "http://openflexo.org/modellers";
 
 	public static final String KAOS_VIEWPOINT_URI = "http://openflexo.org/modellers/Kaos/Kaos.viewpoint";
+
+	public static final String SYSML_KAOS_VIEWPOINT_URI = "http://openflexo.org/modellers/SysMLKaos/SysMLKaos.viewpoint";
 }
