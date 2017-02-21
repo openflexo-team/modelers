3b098d1fb38012e6ad2338097f71ad529a98a5dd
Sylvain Guérin <sylvain@openflexo.org>
2017-02-21 23:16:25 +0100

    MEDIUM / Refactored as API + RC

---
+++ modelers-api/src/main/java/org/openflexo/modelers/ModelersConstants.java
@@ -0,0 +1,9 @@
+package org.openflexo.modelers;
+
+public class ModelersConstants {
+
+	public static final String MODELERS_RC_URI = "http://openflexo.org/modellers";
+
+	public static final String KAOS_VIEWPOINT_URI = "http://openflexo.org/modellers/Kaos/Kaos.viewpoint";
+
+}
--- src/main/java/org/openflexo/modelers/ModelersConstants.java
+++
@@ -1,9 +0,0 @@
-package org.openflexo.modelers;
-
-public class ModelersConstants {
-
-	public static final String MODELERS_RC_URI = "http://openflexo.org/modellers";
-
-	public static final String KAOS_VIEWPOINT_URI = "http://openflexo.org/modellers/Kaos/Kaos.viewpoint";
-
-}
